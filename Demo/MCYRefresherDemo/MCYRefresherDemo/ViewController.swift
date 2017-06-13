1b209b91018ec0bfc8a1f16cf5c673b03b8685d0
miciny <15201114041@163.com>
2017-06-13 11:58:25 +0800

    demo

---
+++ Demo/MCYRefresherDemo/MCYRefresherDemo/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  MCYRefresherDemo
+//
+//  Created by maocaiyuan on 2017/6/13.
+//  Copyright © 2017年 maocaiyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let mainTabelView = UITableView()
+    let Width = UIScreen.main.bounds.width
+    let Height = UIScreen.main.bounds.height
+    var dataSource = [String]()
+    
+    var refreshTime = 0
+    
+    var headerView: RefreshHeaderView?
+    var footerView: LoadMoreView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setData()
+        self.setTableView()
+    }
+    
+    func setTableView(){
+        
+        mainTabelView.frame = CGRect(x: 0, y: 0, width: Width, height: Height)
+        mainTabelView.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
+        mainTabelView.showsHorizontalScrollIndicator = false
+        mainTabelView.tableFooterView = UIView(frame: CGRect.zero)
+        mainTabelView.delegate = self
+        mainTabelView.dataSource = self
+        self.view.addSubview(mainTabelView)
+        
+        headerView =  RefreshHeaderView(subView: mainTabelView, target: self)
+        footerView = LoadMoreView(subView: mainTabelView, target: self)
+        mainTabelView.tableFooterView = footerView
+    }
+    
+    func setData(){
+        dataSource = [String]()
+        for i in refreshTime ..< refreshTime+20{
+            dataSource.append(String(i))
+        }
+        refreshTime += 20
+    }
+    
+    func loadMoreData(){
+        for i in refreshTime ..< refreshTime+20{
+            dataSource.append(String(i))
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    //每个cell内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellId = "cell"
+        let cell =  UITableViewCell(style: .default, reuseIdentifier: cellId)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension ViewController: isRefreshingDelegate{
+    //isfreshing中的代理方法
+    func reFreshing(){
+        
+        self.setData()
+        self.mainTabelView.reloadData()
+        self.headerView?.endRefresh()
+    }
+}
+
+extension ViewController: isLoadMoreDelegate{
+    //isLoadMore中的代理方法
+    func loadingMore(){
+        //这里做你想做的事
+        self.loadMoreData()
+        self.mainTabelView.reloadData()
+        self.footerView?.endRefresh()
+    }
+}
+
--- MCYRefresher.podspec
+++ MCYRefresher.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "MCYRefresher"
-  s.version      = "0.11"
+  s.version      = "1.0" #不能有v
   s.summary      = "MCYRefresher is a very simple refresher and loadMore view"
 
   # This description is used to generate tags and improve search results.
@@ -25,7 +25,7 @@ Pod::Spec.new do |s|
   #   * Write the description between the DESC delimiters below.
   #   * Finally, don't worry about the indent, CocoaPods strips it!
   s.description  = <<-DESC
-                    0.11, as you konw,
+                    v1.0, as you konw,
                     MCYRefresher is a very simple refresher and loadMore view
                    DESC
 
@@ -81,7 +81,7 @@ Pod::Spec.new do |s|
   #  Supports git, hg, bzr, svn and HTTP.
   #
 
-  s.source       = { :git => "https://github.com/miciny/MCYRefresher.git", :tag => "0.111" }
+  s.source       = { :git => "https://github.com/miciny/MCYRefresher.git", :tag => "v1.0" }
 
 
   # ――― Source Code ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
